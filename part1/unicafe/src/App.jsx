b1fddecbe9e8b2a5236ea298a1348664fcdb8710
Ben Wiacek <benjamin.wiacek@gmail.com>
2026-09-22 13:19:38 -0400

    Exercise 1.9 unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,8 +6,18 @@ const Button = ({ onClick, text }) => {
 
 const Statistics = ({ good, neutral, bad}) => {
   const all = good + neutral + bad
-  const average = all=== 0? 0 : (good - bad)/ all
-  const positive = all=== 0? 0 : good / all * 100
+
+  if(all ===0) {
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </>
+    )
+  }
+
+  const average = (good - bad)/ all
+  const positive = good / all * 100
 
   return (
     <>
