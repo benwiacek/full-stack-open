66a7e876e7751a0dedb83eec27e353781c919bd4
Ben Wiacek <benjamin.wiacek@gmail.com>
2026-09-25 00:10:13 -0400

    Exercise 2.9 Phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
-  ]) 
+    {name: 'Arto Hellas', number: '040-123456'}
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -34,9 +32,27 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = filter 
+    ? persons.filter( person => 
+      person.name.toLowerCase().includes(filter.toLowerCase())
+    )
+    : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input
+          value={filter}
+          onChange={handleFilter}
+        />
+      </div>
+
+      <h2>Add a new person</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -57,7 +73,7 @@ const App = () => {
 
       <h2>Numbers</h2>
       <div>
-        {persons.map( person =>
+        {personsToShow.map( person =>
             <div key={person.name}>{person.name} {person.number}</div>
         )}
       </div>
